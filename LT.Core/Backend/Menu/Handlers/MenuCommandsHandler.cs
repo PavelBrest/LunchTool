c2cc252d97d070e9afa3c434bb88b9434424d716
pnov <Pavel.Novikau@epolsoft.com>
2019-10-10 19:56:10 +0300

    Created CommandHandler and MenuMappings for Menu and CreateMenu command with the validator.

---
+++ LT.Core/Backend/Menu/Handlers/MenuCommandsHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using LT.Core.Backend.Decorators;
+using LT.Core.Contracts.Menu.Commands;
+using LT.Core.CQRS;
+using LT.Core.Seedwork.CQRS.Commands;
+using LT.Core.Seedwork.Data;
+using MediatR;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("LT.Core.Tests")]
+namespace LT.Core.Backend.Menu.Handlers
+{
+    [HandlerDecorator(typeof(ValidateRequestDecorator<CreateMenu, Unit>))]
+    internal class MenuCommandsHandler
+        : ICommandHandler<CreateMenu>
+    {
+        private readonly IRepository<Menu> _repository;
+        private readonly IMapper _mapper;
+
+        public MenuCommandsHandler(IRepository<Menu> repository, IMapper mapper)
+        {
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentException(nameof(mapper));
+        }
+
+        public async Task<Unit> Handle(CreateMenu request, CancellationToken cancellationToken)
+        {
+            var entity = _mapper.Map<Menu>(request);
+
+            await _repository.AddAsync(entity);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ LT.Core/Backend/Menu/Mappings/MenuMappings.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using LT.Core.Backend.Dishes;
+using LT.Core.Contracts.Menu.Commands;
+using System.Linq;
+
+namespace LT.Core.Backend.Menu.Mappings
+{
+    internal class MenuMappings : Profile
+    {
+        public MenuMappings()
+        {
+            CreateMap<CreateMenu, Menu>()
+                .ForMember(p => p.PlaceId, cfg => cfg.MapFrom(p => p.PlaceId))
+                .ForMember(p => p.Dishes, cfg => cfg.MapFrom(p => p.Dishes.Select(SelectDish)));
+        }
+
+        private static Dish SelectDish(CreateMenu.DishInfo info)
+        {
+            return new Dish
+            {
+                Description = info.Description,
+                DishTypeId = info.TypeId,
+                Price = info.Price,
+                MenuId = info.MenuId,
+                Id = info.DishId
+            };
+        }
+    }
+}
