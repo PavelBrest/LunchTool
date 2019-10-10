854d8d760a719f286286a267ee6581b6d42cb310
pnov <Pavel.Novikau@epolsoft.com>
2019-10-10 15:21:21 +0300

    Add project files.

---
+++ LT.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LT.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ LT.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LT.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
