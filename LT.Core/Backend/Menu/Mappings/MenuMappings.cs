5250f6abdbcea50b4c524943e46ab6ad70bd1628
pnov <Pavel.Novikau@epolsoft.com>
2019-10-10 20:27:34 +0300

    Added GetMenu query and hanlder.

--- LT.Core/Backend/Menu/Mappings/MenuMappings.cs
+++ LT.Core/Backend/Menu/Mappings/MenuMappings.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using LT.Core.Backend.Dishes;
 using LT.Core.Contracts.Menu.Commands;
+using LT.Core.Contracts.Menu.Views;
 using System.Linq;
 
 namespace LT.Core.Backend.Menu.Mappings
@@ -12,6 +13,22 @@ namespace LT.Core.Backend.Menu.Mappings
             CreateMap<CreateMenu, Menu>()
                 .ForMember(p => p.PlaceId, cfg => cfg.MapFrom(p => p.PlaceId))
                 .ForMember(p => p.Dishes, cfg => cfg.MapFrom(p => p.Dishes.Select(SelectDish)));
+
+            CreateMap<Menu, GetMenuView>()
+                .ForMember(p => p.Dishes, cfg => cfg.MapFrom(p => p.Dishes.Select(SelectDishInfo)));
+
+        }
+
+        private static GetMenuView.DishInfo SelectDishInfo(Dish dish)
+        {
+            return new GetMenuView.DishInfo
+            {
+                DishId = dish.Id,
+                TypeId = dish.DishTypeId,
+                Type = dish.DishType.Title,
+                Description = dish.Description,
+                Price = dish.Price
+            };
         }
 
         private static Dish SelectDish(CreateMenu.DishInfo info)
---
+++ LT.Core/Contracts/Menu/Views/GetMenuView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LT.Core.Contracts.Menu.Views
+{
+    public class GetMenuView
+    {
+        public class DishInfo
+        {
+            public Guid DishId { get; set; }
+            public Guid TypeId { get; set; }
+
+            public string Type { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+        }
+        public Guid Id { get; set; }
+
+        public ICollection<DishInfo> Dishes { get; set; }
+    }
+}
