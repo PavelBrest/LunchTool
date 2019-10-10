aff3e585a38c4901291b6b30b00d0c8ae2e89cca
pnov <Pavel.Novikau@epolsoft.com>
2019-10-10 16:12:35 +0300

    Added query-based and command-based decorators.

---
+++ LT.Core/Seedwork/CQRS/Commands/CommandHandlerDecorator.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LT.Core.Seedwork.CQRS.Commands
+{
+    internal abstract class CommandHandlerDecorator<TCommand>
+        : IPipelineBehavior<TCommand, Unit>
+        where TCommand : class, ICommand
+    {
+        public abstract Task<Unit> Handle(TCommand request, CancellationToken cancellationToken, RequestHandlerDelegate<Unit> next);
+    }
+}
---
+++ LT.Core/Seedwork/CQRS/Query/QueryHandlerDecorator.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LT.Core.Seedwork.CQRS.Query
+{
+    internal abstract class QueryHandlerDecorator<TQuery, Tout>
+        : IPipelineBehavior<TQuery, Tout>
+        where TQuery : class, IQuery<Tout>
+        where Tout : class
+    {
+        public abstract Task<Tout> Handle(TQuery request, CancellationToken cancellationToken, RequestHandlerDelegate<Tout> next);
+    }
+}
