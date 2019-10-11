0ea03be2a0228ad5e8ccc976678038831fe5330b
pnov <Pavel.Novikau@epolsoft.com>
2019-10-11 13:19:37 +0300

    Added LoginUser and RegisterUser commands.

---
+++ LT.Core/Contracts/User/Commands/LoginUser.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using LT.Core.Seedwork.CQRS.Commands;
+
+namespace LT.Core.Contracts.User.Commands
+{
+    public class LoginUser : ICommand
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+
+    internal class LoginUserValidator : AbstractValidator<LoginUser>
+    {
+        public LoginUserValidator()
+        {
+            RuleFor(p => p.Login).NotEmpty()
+                                 .Length(4);
+
+            RuleFor(p => p.Password).NotEmpty()
+                                    .MinimumLength(6)
+                                    .MaximumLength(16);
+        }
+    }
+}
---
+++ LT.Core/Contracts/User/Commands/RegisterUser.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using LT.Core.Seedwork.CQRS.Commands;
+using System;
+
+namespace LT.Core.Contracts.User.Commands
+{
+    public class RegisterUser : ICommand
+    { 
+        public Guid Id { get; set; }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+    }
+
+    internal class RegisterUserValidator : AbstractValidator<RegisterUser>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(p => p.Id).NotEmpty();
+
+            RuleFor(p => p.Login).NotEmpty()
+                                 .Length(4);
+
+            RuleFor(p => p.Name).NotEmpty();
+
+            RuleFor(p => p.EmailAddress).NotEmpty()
+                                        .EmailAddress();
+
+            RuleFor(p => p.Password).NotEmpty()
+                                    .MinimumLength(6)
+                                    .MaximumLength(25);
+        }
+    }
+}
