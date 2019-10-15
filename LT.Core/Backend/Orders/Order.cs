51bc2ad992c3c96ee664e3eb87cbc55a89b38c08
DzmitryBratchuk <bdimmersion@gmail.com>
2019-10-15 04:29:30 -0700

    Update Order sceleton

--- LT.Core/Backend/Orders/Order.cs
+++ LT.Core/Backend/Orders/Order.cs
@@ -1,12 +1,29 @@
 ﻿using LT.Core.Seedwork.Data;
 using System;
+using System.Collections.Generic;
 
 namespace LT.Core.Backend.Orders
 {
     public class Order : IHasId<Guid>
     {
-        public Guid Id => throw new NotImplementedException();
+        object IHasId.Id => Id;
 
-        object IHasId.Id => throw new NotImplementedException();
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+        public Users.User User { get; set; }
+
+        public Guid PlaceId { get; set; }
+        public Places.Place Place { get; set; }
+
+        public ICollection<Dishes.Dish> Dishes { get; set; }
+
+        public Guid LimitId { get; set; }
+        public Limits.Limit Limit { get; set; }
+
+        public DateTime Date { get; set; }
+        public decimal OrderCost { get; set; }
+        public decimal DiscountValue { get; set; }
+        public decimal ResultCost { get; set; }
     }
 }
