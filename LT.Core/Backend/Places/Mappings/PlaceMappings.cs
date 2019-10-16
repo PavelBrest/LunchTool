1fc1cc0b73d39593b3ab16db56f9096eedfabda2
DzmitryBratchuk <bdimmersion@gmail.com>
2019-10-16 03:02:42 -0700

    Fix PlaceMappings

--- LT.Core/Backend/Places/Mappings/PlaceMappings.cs
+++ LT.Core/Backend/Places/Mappings/PlaceMappings.cs
@@ -2,8 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using LT.Core.Contracts.Places.Views;
 using LT.Core.Contracts.Places.Commands;
-using LT.Core.Contracts.Places.Queries;
 
 namespace LT.Core.Backend.Places.Mappings
 {
@@ -14,7 +14,7 @@ namespace LT.Core.Backend.Places.Mappings
             CreateMap<CreatePlace, Place>();
             CreateMap<DeletePlace, Place>();
             CreateMap<UpdatePlace, Place>();
-            CreateMap<Place, GetPlace>();
+            CreateMap<Place, PlaceView>();
         }
     }
 }
