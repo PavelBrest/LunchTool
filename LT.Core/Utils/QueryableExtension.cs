081637ca85bc488a5e4cef292a4eb3466a1dd3c5
Pasha <Pasha.brest.work@gmail.com>
2019-10-17 10:31:11 +0300

    Made QueryableExtension generic.

---
+++ LT.Core/Utils/QueryableExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LT.Core.Utils
+{
+    static class QueryableExtension
+    {
+        public static Task<IReadOnlyList<T>> AsReadOnlyAsync<T>(this IQueryable<T> queryable) =>
+            queryable.ToListAsync()
+                     .ContinueWith(p => (IReadOnlyList<T>)p.Result);
+    }
+}
